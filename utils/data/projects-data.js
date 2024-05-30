f2a8d9794b22aa71855a660969fd6526912b80bb
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-05-30 13:30:43 +0530

    project data added

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -3,6 +3,24 @@ import crefin from "/public/image/crefin.jpg";
 export const projectsData = [
   {
     id: 1,
+    name: "TS-BoilerplateX",
+    description: `"TS-BoilerplateX is a meticulously crafted boilerplate tailored for developers seeking a seamless starting point for building powerful web applications with TypeScript and Express.js. With TS-BoilerplateX, harness the combined benefits of TypeScript's static typing and Express.js' flexibility to expedite your development process while ensuring code integrity and maintainability.`,
+    tools: [
+      "TypeScript",
+      "Express.js",
+      "ESLint ",
+      "Morgan",
+      "bodyParser",
+      "Compression",
+      "CookieParser",
+    ],
+    role: "Backend Developer",
+    code: "",
+    demo: "https://github.com/eldhopaulose/TS-BoilerplateX.git",
+    image: crefin,
+  },
+  {
+    id: 2,
     name: "Nosta",
     description: `"Nosta" is a cutting-edge e-commerce application designed and developed by our skilled team. Utilizing Flutter and GetX, our frontend team crafted a user-friendly interface that ensures seamless navigation and an engaging shopping experience for users across various platforms. The admin application, constructed with ReactJS, provides administrators with a powerful dashboard to efficiently manage products, orders, and users.`,
     tools: ["Flutter", "GetX", "ReactJS", "Dart", "Dio", "Express", "MongoDB"],
